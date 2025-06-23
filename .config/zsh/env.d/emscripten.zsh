0565e8e27530e0736b712640034db49f6df76dca
Ethan Zhang <perfectworks@gmail.com>
2025-06-23 14:58:24 +0800

    Fix environment variable reference in emscripten.zsh to use EMSDK instead of EMSCRIPTEN_ROOT

--- .config/zsh/env.d/emscripten.zsh
+++ .config/zsh/env.d/emscripten.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
-if [ -n "${EMSCRIPTEN_ROOT:-}" && -f "$EMSCRIPTEN_ROOT/emsdk_env.sh" ]; then
-    EMSDK_QUIET=1 source "$EMSCRIPTEN_ROOT/emsdk_env.sh"
+if [ -n "${EMSDK:-}" && -f "$EMSDK/emsdk_env.sh" ]; then
+    EMSDK_QUIET=1 source "$EMSDK/emsdk_env.sh"
 fi
