50c7728229aef4c3da1bd96fdc873b2e6397b1d8
Ethan Zhang <perfectworks@gmail.com>
2025-06-23 15:02:07 +0800

    Fix conditional syntax in emscripten.zsh for improved compatibility

--- .config/zsh/env.d/emscripten.zsh
+++ .config/zsh/env.d/emscripten.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
-if [ -n "${EMSDK:-}" && -f "$EMSDK/emsdk_env.sh" ]; then
+if [[ -n "${EMSDK:-}" && -f "$EMSDK/emsdk_env.sh" ]]; then
     EMSDK_QUIET=1 source "$EMSDK/emsdk_env.sh"
 fi
